6b618bcc5d5ddc5ca4965a16517cdff7a2c27e47
Jaron <jaringson@gmail.com>
2023-06-12 15:15:08 -0600

    run_monte_carlo.py making right amount of spaces

--- run_monte_carlo.py
+++ run_monte_carlo.py
@@ -19,15 +19,15 @@ for i in range(0, 1000):
     num_quads = np.random.randint(10,40)
     if params.is_2d:
         num_quads = np.random.randint(10,20)
-	collision_range = np.random.random() * 100
-	max_vel = 5.0 + np.random.random() * 5.0
-	outfile = 'hardData5/run'+str(i)+'quads'+str(num_quads)+'cr'+str(trunc(collision_range))+'mv'+str(trunc(max_vel))+'.json'
-	# current_sim = Sim()
-	print(outfile)
-	sim.run_sim(num_quads, collision_range, max_vel, outfile)
+    collision_range = np.random.random() * 100
+    max_vel = 5.0 + np.random.random() * 5.0
+    outfile = 'hardData5/run'+str(i)+'quads'+str(num_quads)+'cr'+str(trunc(collision_range))+'mv'+str(trunc(max_vel))+'.json'
+    # current_sim = Sim()
+    print(outfile)
+    sim.run_sim(num_quads, collision_range, max_vel, outfile)
 
-	reload(sim)
-	# set_trace()
+    reload(sim)
+    # set_trace()
 
 #if __name__ == "__main__":
 #	run_monte_carlo(arg[0])
