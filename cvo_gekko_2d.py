fa51fdd7daf2d77eee1154be98f45cf44c103a06
Jaron <jaringson@gmail.com>
2023-06-23 01:42:47 -0600

    new method for velocity uncertainty

--- cvo_gekko_2d.py
+++ cvo_gekko_2d.py
@@ -78,6 +78,8 @@ class CVOGekko:
             ''' Position Uncertainty '''
             a = uncertaintyPos[i][0]+2.0*self.collisionRadius
             b = uncertaintyPos[i][1]+2.0*self.collisionRadius
+            j_pos = from1XTo2X[0,0]
+            k_pos = from1XTo2X[1,0]
 
             start_a = a
             start_b = b
@@ -86,8 +88,6 @@ class CVOGekko:
 
             while apexInEllipsoid:
 
-                j_pos = from1XTo2X[0,0]
-                k_pos = from1XTo2X[1,0]
                 if j_pos**2/a**2 + k_pos**2/b**2 - 1.0 < 0:
                     failed = True
 
@@ -107,60 +107,58 @@ class CVOGekko:
             ''' Velocity Uncertainty '''
             uVx = uncertaintyVel[i][0]
             uVy = uncertaintyVel[i][1]
-            s_norm = m.Intermediate(m.sqrt((sx-apexOfCollisionCone[0,0])**2 + (sy-apexOfCollisionCone[1,0])**2))
-            x_trans = m.Intermediate(uVx * (sx-apexOfCollisionCone[0,0]) / s_norm)
-            y_trans = m.Intermediate(uVy * (sy-apexOfCollisionCone[1,0]) / s_norm)
+            nx = 1
+            ny = k_pos*a**2/(j_pos*b**2)
+            proj_len = m.Intermediate( (sx*nx+sy*ny)/(nx*nx+ny*ny) )
+            proj_x = m.Intermediate(sx - nx*proj_len)
+            proj_y = m.Intermediate(sy - ny*proj_len)
+            proj_norm = m.Intermediate(m.sqrt(proj_x**2 + proj_y**2))
+            x_trans = m.Intermediate(uVx * proj_x / proj_norm)
+            y_trans = m.Intermediate(uVy * proj_y / proj_norm)
 
             ''' Make sure the point is not inside velocity uncertainty '''
             m.Equation( (sx-apexOfCollisionCone[0,0])**2/uVx**2 +
-                                (sy-apexOfCollisionCone[1,0])**2/uVy**2 - 1 >= 0 )
+                                (sy-apexOfCollisionCone[1,0])**2/uVy**2 - 1.0 >= 0 )
 
-            apx = apexOfCollisionCone[0,0] + x_trans
-            apy = apexOfCollisionCone[1,0] + y_trans
+            all_x_trans = [0, x_trans]
+            all_y_trans = [0, y_trans]
 
-            j = centerOfEllipsoid[0,0] + x_trans
-            k = centerOfEllipsoid[1,0] + y_trans
+            for i in range(len(all_x_trans)):
+                apx = apexOfCollisionCone[0,0] + all_x_trans[i]
+                apy = apexOfCollisionCone[1,0] + all_y_trans[i]
+
+                j = centerOfEllipsoid[0,0] + all_x_trans[i]
+                k = centerOfEllipsoid[1,0] + all_y_trans[i]
 
 
-            new_s = np.array([
-                    [sx-apx],
-                    [sy-apy],
-                    [0.0]])
+                new_s = np.array([
+                        [sx-apx],
+                        [sy-apy],
+                        [0.0]])
 
-            M = np.array([ [1.0/(a*a), 0.0, -j/(a*a)],
-                [0.0, 1.0/(b*b), -k/(b*b)],
-                [-j/(a*a), -k/(b*b), j*j/(a*a)+k*k/(b*b)-1.0] ])
+                M = np.array([ [1.0/(a*a), 0.0, -j/(a*a)],
+                    [0.0, 1.0/(b*b), -k/(b*b)],
+                    [-j/(a*a), -k/(b*b), j*j/(a*a)+k*k/(b*b)-1.0] ])
 
-            ap = np.array([[apx], [apy], [1.0]])
+                ap = np.array([[apx], [apy], [1.0]])
 
-            lam1 = m.Intermediate((-new_s.T@M@ap)[0,0])
-            lam2 = m.Intermediate((new_s.T@M@new_s)[0,0])
-            # lam = m.Intermediate(m.sqrt( lam1*lam1/(lam2*lam2) ))
-            lam = m.Intermediate(m.abs2(lam1/lam2))
+                lam1 = m.Intermediate((-new_s.T@M@ap)[0,0])
+                lam2 = m.Intermediate((new_s.T@M@new_s)[0,0])
+                # lam = m.Intermediate(m.sqrt( lam1*lam1/(lam2*lam2) ))
+                lam = m.Intermediate(m.abs2(lam1/lam2))
 
-            valx = apx+lam*(sx-apx)
-            valy = apy+lam*(sy-apy)
+                valx = apx+lam*(sx-apx)
+                valy = apy+lam*(sy-apy)
 
-            val = np.array([ [valx], [valy], [1.0] ])
-            # set_trace()
+                val = np.array([ [valx], [valy], [1.0] ])
+                # set_trace()
 
-            constraint = m.Intermediate((val.T@M@val)[0,0])
-            # allContraints.append(constraint)
+                constraint = m.Intermediate((val.T@M@val)[0,0])
+                # allContraints.append(constraint)
 
-            #delta = m.Var()
-            #allDeltas.append(delta)
-            #
-            #x1 = self.collisionRadius
-            #x2 = self.collisionRange
-            #m_line = (y2-y1)/(x2-x1)
-            #b_line = y1-m_line*x1
-            #weight = m.Intermediate(m_line*norm(from1XTo2X)+b_line)
-            # weight = 1e6*self.collisionRange*1.0/(norm(from1XTo2X)+self.collisionRadius-0.1)
 
-            #m.Equation( constraint + delta >= 0  )
-            #m.Obj(delta * delta * weight)
 
-            m.Equation( constraint >= 0  )
+                m.Equation( constraint >= 0  )
 
 
 
